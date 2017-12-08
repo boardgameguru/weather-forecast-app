1afa071c353fbc75be583e0ae00a2069d3f80c0c
Thomas Spencer <t.spencer1990@hotmail.com>
2017-12-08 15:56:42 -0500

    Added two controllers, homeController and forecastController, and created two templates for the controllers in the pages folder.

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 //Module
 var weatherApp = angular.module('weatherApp', ['ngResource', 'ngRoute']);
+
+//Controllers
+
+weatherApp.controller('homeController', ['$scope', function($scope) {
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope) {
+
+}]);
