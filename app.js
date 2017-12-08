2fa2f2a6158078fde9701cee683a0c9da04caf37
Thomas Spencer <t.spencer1990@hotmail.com>
2017-12-08 16:02:11 -0500

    Added routing for my single page application.

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 //Module
 var weatherApp = angular.module('weatherApp', ['ngResource', 'ngRoute']);
 
+//Routes
+weatherApp.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'pages/home.htm'
+    controller: 'homeController'
+  })
+
+  .when('/forecast', {
+    templateUrl: 'pages/forecast.htm'
+    controller: 'forecastController'
+  })
+})
+
 //Controllers
 
 weatherApp.controller('homeController', ['$scope', function($scope) {
