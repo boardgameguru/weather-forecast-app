f5ef911fb48b9c2b945b2540bb83fab7a5801b74
Thomas Spencer <t.spencer1990@hotmail.com>
2017-12-10 17:06:29 -0500

    Added an additional route to accept a parameter to allow users to request multiple days of forecasts. Injected $routeParams to my forecast controller to allow for the retrieval of the day parameter and stored it in $scope.days

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ weatherApp.config(function($routeProvider) {
     templateUrl: 'pages/forecast.html',
     controller: 'forecastController'
   })
+
+  .when('/forecast/:days', {
+      templateUrl: 'pages/forecast.html',
+      controller: 'forecastController'
+  })
 });
 
 //Services
@@ -35,10 +40,12 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource ,cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
     $scope.city = cityService.city
 
-    var currentWeatherAPI = $resource('http://api.openweathermap.org/data/2.5/weather?q=:city&appid=:appid', { city: $scope.city, appid: config.weatherAPIKey });
+    $scope.days = $routeParams.days || '1'
+
+    var currentWeatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast?q=:city&cnt=:days&appid=:appid', { city: $scope.city, days: $scope.days , appid: config.weatherAPIKey });
 
     $scope.currentWeather = currentWeatherAPI.get();
 
